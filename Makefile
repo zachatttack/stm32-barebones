cc6419d5bfa9c9269da32fe046ccc4d93122cd7d
Zach Thomas <zach.thomas1.zt@gmail.com>
2022-06-29 08:47:47 -0500

    blinky working for f401 with my linker script and startup file

--- Makefile
+++ Makefile
@@ -1,17 +1,25 @@
 CC=arm-none-eabi-gcc
 MACH=cortex-m4
 CFLAGS= -c -mcpu=$(MACH) -mthumb -std=gnu11 -Wall
+LDFLAGS= -nostdlib -T f401.ld -Wl,-Map=final.map
+OCDCFG = -f /usr/share/openocd/scripts/interface/stlink-v2.cfg -f /usr/share/openocd/scripts/target/stm32f4x.cfg
 
-all: main.o led.o stm32_startup.o
+all: blink.o stm32_startup.o blink.elf
 
-main.o: main.c
-	$(CC) $(CFLAGS) -o $@ $^
-
-led.o: led.c
+blink.o: blink.c
 	$(CC) $(CFLAGS) -o $@ $^
 
 stm32_startup.o: stm32_startup.c
 	$(CC) $(CFLAGS) -o $@ $^
 
+blink.elf: blink.o stm32_startup.o
+	$(CC) $(LDFLAGS) -o $@ $^
+
 clean:
 	rm -rf *.o *.elf
+
+flash: blink.elf
+	openocd $(OCDCFG) -c "program blink.elf verify reset exit"
+
+ocd:
+	openocd $(OCDCFG)
--- stm32_startup.c
+++ stm32_startup.c
@@ -1,10 +1,19 @@
 #include <stdint.h>
 
 #define SRAM_START 0x20000000U
-#define SRAM_SIZE (128 * 1024) //128KB
+#define SRAM_SIZE (96 * 1024) //96K
 #define SRAM_END ((SRAM_START) + (SRAM_SIZE)) //0x20020000U
 
 #define STACK_START SRAM_END
+
+extern uint32_t _etext;
+extern uint32_t _sdata;
+extern uint32_t _edata;
+
+extern uint32_t _sbss;
+extern uint32_t _ebss;
+
+void main(void);
 
 void Reset_Handler(void);
 void NMI_Handler(void)            __attribute__((weak, alias ("Default_Handler")));
@@ -51,5 +60,20 @@ void Default_Handler(void)
 
 void Reset_Handler(void)
 {
+    uint32_t size = (uint32_t)&_edata - (uint32_t)&_sdata;
+    uint8_t *pDst = (uint8_t*)&_sdata; //sram
+    uint8_t *pSrc = (uint8_t*)&_etext; //flash
+
+    for(uint32_t i=0; i < size ; i++){
+      *pDst++ = *pSrc++;
+    }
+
+    size = &_ebss - &_sbss;
+    pDst = (uint8_t*)&_sbss;
+    for(uint32_t i=0; i < size ; i++){
+      *pDst++ = 0;
+    }
+
+    main();
 
 }
